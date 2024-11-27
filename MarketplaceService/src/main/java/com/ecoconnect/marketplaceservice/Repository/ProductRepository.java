d8f81ce831e0a7400f6301682c29fb8443a23cba
pallavivangari <vangaripallavi27@gmail.com>
2024-11-26 16:20:36 -0800

    removed unnecessary code

--- MarketplaceService/src/main/java/com/ecoconnect/marketplaceservice/Repository/ProductRepository.java
+++ MarketplaceService/src/main/java/com/ecoconnect/marketplaceservice/Repository/ProductRepository.java
@@ -10,6 +10,4 @@ import java.util.Optional;
 @Repository
 public interface ProductRepository extends MongoRepository<Product, String> {
 
-    Optional<Product> findById(String id);
-    List<Product> findAll();
 }
