39ed337cd93eca7198989497a68a236f50f02563
pallavivangari <vangaripallavi27@gmail.com>
2025-02-16 13:39:58 -0500

    Refactored code to include json mapping

--- UserService/src/main/java/com/ecoconnect/userservice/Service/UserPublisher.java
+++ UserService/src/main/java/com/ecoconnect/userservice/Service/UserPublisher.java
@@ -1,38 +1,54 @@
 package com.ecoconnect.userservice.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class UserPublisher {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
 
     @Autowired
-    public UserPublisher(KafkaTemplate<String, String> kafkaTemplate) {
+    public UserPublisher(KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper) {
         this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
     }
 
-    // Method to publish "USER_FOLLOWED" event to Kafka
+    // Publish "USER_FOLLOWED" event
     public void publishUserFollowed(String followerId, String followeeId, String followerEmail, String followeeEmail) {
-        String message = String.format(
-                "{\"messageType\":\"USER_FOLLOWED\",\"followerId\":\"%s\",\"followeeId\":\"%s\",\"followerEmail\":\"%s\",\"followeeEmail\":\"%s\"}",
-                followerId, followeeId, followerEmail, followeeEmail
-        );
+        Map<String, Object> message = new HashMap<>();
+        message.put("messageType", "USER_FOLLOWED");
+        message.put("followerId", followerId);
+        message.put("followeeId", followeeId);
+        message.put("followerEmail", followerEmail);
+        message.put("followeeEmail", followeeEmail);
 
-        // Send the message to the "user-notifications" Kafka topic
-        kafkaTemplate.send("user-notifications", message);
+        sendMessage("user-notifications", message);
     }
 
-    // Method to publish "USER_UNFOLLOWED" event to Kafka
+    // Publish "USER_UNFOLLOWED" event
     public void publishUserUnfollowed(String followerId, String followeeId) {
-        String message = String.format(
-                "{\"messageType\":\"USER_UNFOLLOWED\",\"followerId\":\"%s\",\"followeeId\":\"%s\"}",
-                followerId, followeeId
-        );
+        Map<String, Object> message = new HashMap<>();
+        message.put("messageType", "USER_UNFOLLOWED");
+        message.put("followerId", followerId);
+        message.put("followeeId", followeeId);
 
-        // Send the message to the "user-notifications" Kafka topic
-        kafkaTemplate.send("user-notifications", message);
+        sendMessage("user-notifications", message);
+    }
+
+    // Generic method to send JSON messages
+    private void sendMessage(String topic, Map<String, Object> message) {
+        try {
+            String jsonMessage = objectMapper.writeValueAsString(message);
+            kafkaTemplate.send(topic, jsonMessage);
+        } catch (Exception e) {
+            throw new RuntimeException("Error serializing Kafka message", e);
+        }
     }
 }
