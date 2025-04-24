48046ba6b887a31de99e4ccc8c358001cd9ed70e
Keerthana <keerthanaparsa23@gmail.com>
2025-04-24 12:20:26 -0700

    docker-compose.prod.yml update

--- Frontend/src/config/config.ts
+++ Frontend/src/config/config.ts
@@ -1,10 +1,10 @@
 const Config = {
-    USER_SERVICE_URL: 'http://3.208.31.213:8088/api/users',
-    POST_SERVICE_URL: 'http://3.208.31.213:8088/api/posts',
-    NOTIFICATION_SERVICE_URL: 'http://3.208.31.213:8088/api/notifications/preferences',
-    MARKETPLACE_SERVICE_URL: 'http://3.208.31.213:8088/api/marketplace',
-    FEED_SERVICE_URL: 'http://3.208.31.213:8088/api/feed',
-    EVENT_SERVICE_URL: 'http://3.208.31.213:8088/api/events',
+    USER_SERVICE_URL: 'http://54.172.233.110:8088/api/users',
+    POST_SERVICE_URL: 'http://54.172.233.110:8088/api/posts',
+    NOTIFICATION_SERVICE_URL: 'http://54.172.233.110:8088/api/notifications/preferences',
+    MARKETPLACE_SERVICE_URL: 'http://54.172.233.110:8088/api/marketplace',
+    FEED_SERVICE_URL: 'http://54.172.233.110:8088/api/feed',
+    EVENT_SERVICE_URL: 'http://54.172.233.110:8088/api/events',
     CHATBOT_SERVICE_URL: 'http://127.0.0.1:5000/api/chat',
 };
 
