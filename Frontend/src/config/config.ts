7e17e29d4c6f2f1e0ced1ea1fe0efbde871db3c2
Keerthana <keerthanaparsa23@gmail.com>
2025-04-25 00:56:50 -0700

    deployment changes

--- Frontend/src/config/config.ts
+++ Frontend/src/config/config.ts
@@ -1,11 +1,11 @@
 const Config = {
-    USER_SERVICE_URL: 'http://54.144.232.35:8088/api/users',
-    POST_SERVICE_URL: 'http://54.144.232.35:8088/api/posts',
-    NOTIFICATION_SERVICE_URL: 'http://54.144.232.35:8088/api/notifications/preferences',
-    MARKETPLACE_SERVICE_URL: 'http://54.144.232.35:8088/api/marketplace',
-    FEED_SERVICE_URL: 'http://54.144.232.35:8088/api/feed',
-    EVENT_SERVICE_URL: 'http://54.144.232.35:8088/api/events',
-    CHATBOT_SERVICE_URL: 'http://127.0.0.1:5000/api/chat',
+    USER_SERVICE_URL: 'https://54.144.232.35:8088/api/users',
+    POST_SERVICE_URL: 'https://54.144.232.35:8088/api/posts',
+    NOTIFICATION_SERVICE_URL: 'https://54.144.232.35:8088/api/notifications/preferences',
+    MARKETPLACE_SERVICE_URL: 'https://54.144.232.35:8088/api/marketplace',
+    FEED_SERVICE_URL: 'https://54.144.232.35:8088/api/feed',
+    EVENT_SERVICE_URL: 'https://54.144.232.35:8088/api/events',
+    CHATBOT_SERVICE_URL: 'https://127.0.0.1:5000/api/chat',
 };
 
 export default Config;
