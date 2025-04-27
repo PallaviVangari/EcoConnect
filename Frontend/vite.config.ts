cd61ad80f642cc90e6727bb682040f6dbc8d2c2d
Keerthana <37702583+ParsaKeerthana@users.noreply.github.com>
2025-04-26 17:24:44 -0700

    Revert "pr"

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -3,15 +3,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    // "process.env": process.env,
-    // // By default, Vite doesn't include shims for NodeJS/
-    // // necessary for segment analytics lib to work
-    "global": {},
-  },
+define: {
+        // "process.env": process.env,
+        // // By default, Vite doesn't include shims for NodeJS/
+        // // necessary for segment analytics lib to work
+        "global": {},
+    },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-
 });
